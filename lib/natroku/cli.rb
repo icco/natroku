87dccb14305e968a6beb85ac3bd72ee99b2b2298
Nat Welch <nat@natwelch.com>
2014-07-28 21:44:12 +0000

    Valid descriptions

--- lib/natroku/cli.rb
+++ lib/natroku/cli.rb
@@ -2,14 +2,17 @@ module Natroku
   class Cli < Thor
     include Thor::Actions
 
+    desc "start user/repo", "Start a server based on a location spec"
     def start spec
     end
 
+    desc "restart user/repo", "Restart a server based on a location spec"
     def restart spec
       stop spec
       start spec
     end
 
+    desc "stop user/repo", "Stop a server based on a location spec"
     def stop spec
     end
   end
