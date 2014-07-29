7eb1af128d0a69f786e0d3b0130c1a069327a089
Nat Welch <nat@natwelch.com>
2014-07-29 08:37:05 +0000

    server updates

--- lib/natroku/cloud/server.rb
+++ lib/natroku/cloud/server.rb
@@ -6,6 +6,9 @@ module Natroku
           raise "Spec must be a valid Spec."
         end
 
+        package_list = File.readlines('packages.txt').map {|l| l.strip }
+        script = StartupScript.new(HOSTNAME, package_list, USERS).to_s
+
         connection = Fog::Compute.new({ :provider => "Google" })
         name = spec.valid_cloud_name
 
@@ -19,14 +22,18 @@ module Natroku
         disk.wait_for { disk.ready? }
 
         server = connection.servers.bootstrap{
-          :name => name
-          :disks => [disk],
+          :name => name,
+          :disks => [ disk.get_as_boot_disk(true), ],
           :machine_type => Natroku::Cloud::MACHINE_TYPE,
           :zone_name => Natroku::Cloud::ZONE,
           :private_key_path => File.expand_path("~/.ssh/id_rsa"),
           :public_key_path => File.expand_path("~/.ssh/id_rsa.pub"),
           :user => ENV['USER'],
-          :tags => ["fog", "natroku"]
+          :tags => ["fog", "natroku"],
+          :metadata => {
+            # https://developers.google.com/compute/docs/howtos/startupscript#example
+            'startup-script' => script,
+          },
         })
         server.wait_for? { ready? }
 
