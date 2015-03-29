413761beed4b7126abd93efa1919dc45026aec72
Nat Welch <nat@natwelch.com>
2015-03-29 20:51:30 +0000

    purge

--- lib/natroku.rb
+++
@@ -1,9 +0,0 @@
-require "thor"
-
-require "natroku/cli"
-require "natroku/cloud"
-require "natroku/spec"
-require "natroku/version"
-
-module Natroku
-end
--- lib/natroku/version.rb
+++
@@ -1,3 +0,0 @@
-module Natroku
-  VERSION = "0.0.1"
-end
